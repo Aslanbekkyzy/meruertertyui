0e30069b6d97b71db9def92fe6830f3c697dac50
Aslanbekkyzy <104725130+Aslanbekkyzy@users.noreply.github.com>
2024-10-16 18:51:11 +0500

    Первоначальные файлы для проекта MakeCode

---
+++ main.py
@@ -0,0 +1,4 @@
+
+controller.move_sprite(None)
+music.string_playable("", 120)
+scene.set_background_color(234)
---
+++ main.ts
@@ -0,0 +1,3 @@
+controller.moveSprite(null)
+music.stringPlayable("", 120)
+scene.setBackgroundColor(234)
